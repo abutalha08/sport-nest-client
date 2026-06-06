fef2c269a640b40552398341010cc6c15ef6cb94
abutalha08 <taufique752@gmail.com>
2026-06-07 02:18:26 +0600

    Add metadata in RegisterPage

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { ArrowRight, ArrowLeft, ShieldCheck } from "lucide-react";
 
@@ -25,6 +25,10 @@ import Link from "next/link";
 const RegisterPage = () => {
 
   const router = useRouter();
+
+   useEffect(() => {
+    document.title = "Register | SportNest";
+  }, []);
 
     const onSubmit = async (e) => {
     e.preventDefault();
