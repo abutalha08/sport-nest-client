e5c15aaa02785cbea1f9cbc8a71d9c1e37e007e4
abutalha08 <taufique752@gmail.com>
2026-06-07 13:24:11 +0600

    Solve all types small issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,9 +33,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-
-        <Navbar></Navbar>
-        
+          <Navbar></Navbar>    
         {children}
 
         <Toaster />
